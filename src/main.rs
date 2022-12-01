40a9ac9e202678fdeb9bb6e08bee07c0676be26a
Degover <chaors2000@gmail.com>
2022-12-01 11:53:32 -0300

    Small fixes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::io;
 
+pub mod day_01;
+
 fn main() {
     println!("Choose a day");
 
@@ -16,21 +18,23 @@ fn main() {
         .read_line(&mut part)
         .expect("Failed to read line");
 
-    let input = read_input(&day);
-    let result = run_solution(input, &day, &part);
+    let input = read_input(&day.trim());
+    let result = run_solution(input, &day.trim(), &part.trim());
     println!("{}", result)
 }
 
 fn run_solution(input: String, day: &str, part: &str) -> String {
     match day {
-        "1" => "solution",
-        _ => "This day was not solved yet!",
+        "1" => match part {
+            "1" => day_01::part_1(input),
+            _ => "There's no corresponding part to solve".to_string(),
+        },
+        _ => "This day was not solved yet!".to_string(),
     }
-    .to_string()
 }
 
 fn read_input(day: &str) -> String {
-    let file_path = format!("../inputs/day_{day}.txt");
+    let file_path = format!("inputs/day_{day}.txt");
 
     return fs::read_to_string(file_path).expect("Should have been able to read the file");
 }
