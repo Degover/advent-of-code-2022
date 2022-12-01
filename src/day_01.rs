de2df6ee3a5a13932252dfa4d4ef593673c4983f
Degover <chaors2000@gmail.com>
2022-12-01 11:53:28 -0300

    Resolved day 01, part 1

---
+++ src/day_01.rs
@@ -0,0 +1,24 @@
+pub fn part_1(input: String) -> String {
+    let mut curr_calories = 0;
+    let mut most_calories = 0;
+
+    for line in input.lines() {
+        if line == "" {
+            if curr_calories > most_calories {
+                most_calories = curr_calories;
+            }
+
+            curr_calories = 0;
+            continue;
+        }
+
+        let calory: i32 = line.parse().expect("Expected a number");
+        curr_calories += calory;
+    }
+
+    if curr_calories > most_calories {
+        most_calories = curr_calories;
+    }
+
+    return most_calories.to_string();
+}
