7284df41c2e6567eb958895a6ad34cd8c32b319d
Degover <chaors2000@gmail.com>
2022-12-05 18:07:14 -0300

    Added files to solve day 5, with empty funcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_05;
 
 pub type SolutionFn = fn(String) -> String;
 
@@ -13,6 +14,7 @@ pub fn get_solution(day: &u8) -> (SolutionFn, SolutionFn) {
         1 => (day_01::part_1::solve, day_01::part_2::solve),
         2 => (day_02::part_1::solve, day_02::part_2::solve),
         3 => (day_03::part_1::solve, day_03::part_2::solve),
+        5 => (day_05::part_1::solve, day_05::part_2::solve),
         _ => {
             println!("This day has no solution");
             (not_implemented, not_implemented)
