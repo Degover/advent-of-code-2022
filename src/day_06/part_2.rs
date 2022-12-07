ee0d60a27112e9f38d8dda5a40257b7cb0bedd64
Degover <chaors2000@gmail.com>
2022-12-06 22:17:55 -0300

    Added files to solve day 06

---
+++ src/day_06/part_2.rs
@@ -0,0 +1,17 @@
+pub fn solve(_input: String) -> String {
+    return "Not solved yet!".to_string();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn solve_should_be_correct() {
+        let example = "something".to_string();
+
+        let result = solve(example);
+
+        assert_eq!(result, "Not solved yet!");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod day_01;
 mod day_02;
 mod day_03;
 mod day_05;
+mod day_06;
 
 pub type SolutionFn = fn(String) -> String;
 
@@ -15,6 +16,7 @@ pub fn get_solution(day: &u8) -> (SolutionFn, SolutionFn) {
         2 => (day_02::part_1::solve, day_02::part_2::solve),
         3 => (day_03::part_1::solve, day_03::part_2::solve),
         5 => (day_05::part_1::solve, day_05::part_2::solve),
+        6 => (day_06::part_1::solve, day_06::part_2::solve),
         _ => {
             println!("This day has no solution");
             (not_implemented, not_implemented)
