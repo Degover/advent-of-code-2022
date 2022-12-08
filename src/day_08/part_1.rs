b158222fddfa9b906293624cf22039dfedf09c93
Degover <chaors2000@gmail.com>
2022-12-08 13:30:48 -0300

    Resolved day 08

--- src/day_08/part_1.rs
+++ src/day_08/part_1.rs
@@ -1,5 +1,43 @@
-pub fn solve(_input: String) -> String {
-    return "Not solved yet!".to_string();
+pub fn solve(input: String) -> String {
+    let three_grid: Vec<Vec<u32>> = input
+        .lines()
+        .map(|line| -> Vec<u32> { line.chars().map(|c| c.to_digit(10).unwrap()).collect() })
+        .collect();
+    let last_row_index = three_grid.len() - 1;
+
+    let mut visible_count = 0;
+    for (row_index, row) in three_grid.iter().enumerate() {
+        let last_column_index = row.len() - 1;
+        if row_index == 0 || row_index == last_row_index {
+            visible_count += last_column_index + 1;
+            continue;
+        }
+
+        for (column_index, three_height) in row.iter().enumerate() {
+            if column_index == 0 || column_index == last_row_index {
+                visible_count += 1;
+                continue;
+            }
+
+            if !row[0..column_index].iter().any(|x| x >= three_height)
+                || !row[column_index + 1..last_column_index + 1]
+                    .iter()
+                    .any(|x| x >= three_height)
+                || !three_grid[0..row_index]
+                    .iter()
+                    .map(|r| r[column_index])
+                    .any(|x| x >= *three_height)
+                || !three_grid[row_index + 1..last_row_index + 1]
+                    .iter()
+                    .map(|r| r[column_index])
+                    .any(|x| x >= *three_height)
+            {
+                visible_count += 1;
+            }
+        }
+    }
+
+    return visible_count.to_string();
 }
 
 #[cfg(test)]
@@ -8,10 +46,10 @@ mod test {
 
     #[test]
     fn solve_should_be_correct() {
-        let example = "some-example".to_string();
+        let example = ["30373", "25512", "65332", "33549", "35390"].join("\n");
 
         let result = solve(example);
 
-        assert_eq!(result, "Not solved yet!")
+        assert_eq!(result, "21")
     }
 }
--- src/day_08/part_2.rs
+++ src/day_08/part_2.rs
@@ -1,5 +1,55 @@
-pub fn solve(_input: String) -> String {
-    return "Not solved yet!".to_string();
+pub fn solve(input: String) -> String {
+    let three_grid: Vec<Vec<u32>> = input
+        .lines()
+        .map(|line| -> Vec<u32> { line.chars().map(|c| c.to_digit(10).unwrap()).collect() })
+        .collect();
+    let last_row_index = three_grid.len() - 1;
+
+    let mut highest_score = 0;
+    for (row_index, row) in three_grid.iter().enumerate() {
+        let last_column_index = row.len() - 1;
+        if row_index == 0 || row_index == last_row_index {
+            continue;
+        }
+
+        for (column_index, three_height) in row.iter().enumerate() {
+            if column_index == 0 || column_index == last_row_index {
+                continue;
+            }
+
+            let left_score = row[0..column_index]
+                .iter()
+                .rev()
+                .position(|x| x >= three_height)
+                .unwrap_or(column_index - 1)
+                + 1;
+            let right_score = row[column_index + 1..last_column_index + 1]
+                .iter()
+                .position(|x| x >= three_height)
+                .unwrap_or(last_column_index - column_index - 1)
+                + 1;
+            let up_score = three_grid[0..row_index]
+                .iter()
+                .rev()
+                .map(|r| r[column_index])
+                .position(|x| x >= *three_height)
+                .unwrap_or(row_index - 1)
+                + 1;
+            let down_score = three_grid[row_index + 1..last_row_index + 1]
+                .iter()
+                .map(|r| r[column_index])
+                .position(|x| x >= *three_height)
+                .unwrap_or(last_row_index - row_index - 1)
+                + 1;
+
+            let total_score = left_score * right_score * up_score * down_score;
+            if total_score > highest_score {
+                highest_score = total_score;
+            }
+        }
+    }
+
+    return highest_score.to_string();
 }
 
 #[cfg(test)]
@@ -8,10 +58,10 @@ mod test {
 
     #[test]
     fn solve_should_be_correct() {
-        let example = "some-example".to_string();
+        let example = ["30373", "25512", "65332", "33549", "35390"].join("\n");
 
         let result = solve(example);
 
-        assert_eq!(result, "Not solved yet!")
+        assert_eq!(result, "8")
     }
 }
