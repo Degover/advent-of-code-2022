989b3ffeed48fa001904014c5abaa3d6be37ad10
Degover <chaors2000@gmail.com>
2022-12-11 18:58:04 -0300

    Resolved day 11, part 01

---
+++ src/day_11/commons.rs
@@ -0,0 +1,125 @@
+type MonkeyOperator = Box<dyn Fn(&u64) -> u64>;
+
+pub struct Monkey {
+    pub items: Vec<u64>,
+    pub operate: MonkeyOperator,
+    pub divisible_num: u64,
+    pub on_true_target: usize,
+    pub on_false_target: usize,
+}
+
+impl Monkey {
+    pub fn dummy() -> Self {
+        Self {
+            items: Vec::new(),
+            operate: Box::new(|old| old * 1),
+            divisible_num: 0,
+            on_true_target: 0,
+            on_false_target: 0,
+        }
+    }
+
+    pub fn parse(input: &str) -> Self {
+        let mut lines: Vec<&str> = input.lines().collect();
+
+        let on_false_target = parse_last_num(lines.pop().expect("Expected a false condition line"))
+            .try_into()
+            .unwrap();
+
+        let on_true_target = parse_last_num(lines.pop().expect("Expected a true condition line"))
+            .try_into()
+            .unwrap();
+
+        let divisible_num = parse_last_num(lines.pop().expect("Expected a divisible by line"));
+        let operate = parse_operation(lines.pop().expect("Expected a operations line"));
+        let items = parse_items(lines.pop().expect("Expected a items line"));
+
+        Self {
+            items,
+            operate,
+            divisible_num: divisible_num as u64,
+            on_true_target,
+            on_false_target,
+        }
+    }
+}
+
+fn parse_items(items_line: &str) -> Vec<u64> {
+    return items_line
+        .replace("Starting items:", "")
+        .split(",")
+        .map(|num| -> u64 {
+            num.trim()
+                .parse()
+                .expect(&format!("Expected a number, got {num}"))
+        })
+        .collect();
+}
+
+fn parse_operation(operation_line: &str) -> MonkeyOperator {
+    let mut splitted: Vec<&str> = operation_line.split_whitespace().collect();
+    let argument = splitted.pop().expect("Expected a final argument");
+    let operator = splitted.pop().expect("Expected a operator");
+
+    match operator {
+        "+" => {
+            if argument == "old" {
+                return Box::new(move |&old| old + old);
+            } else {
+                let number_argument: u64 = argument
+                    .parse()
+                    .expect(&format!("Expected a number, got {argument}"));
+                return Box::new(move |&old| old + number_argument);
+            }
+        }
+        "*" => {
+            if argument == "old" {
+                return Box::new(move |&old| old * old);
+            } else {
+                let number_argument: u64 = argument
+                    .parse()
+                    .expect(&format!("Expected a number, got {argument}"));
+                return Box::new(move |&old| old * number_argument);
+            }
+        }
+        other => panic!("Expected a valid argument, got {other}"),
+    }
+}
+
+fn parse_last_num(line: &str) -> i32 {
+    let error_message = &format!("Expected a number at the end of the line: '{line}'");
+
+    return line
+        .split_whitespace()
+        .last()
+        .expect(error_message)
+        .parse()
+        .expect(error_message);
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{day_11::commons::Monkey, NEW_LINE};
+
+    #[test]
+    fn monkey_parse_should_be_correct() {
+        let example = [
+            "Monkey 1:",
+            "  Starting items: 54, 65, 75, 74",
+            "  Operation: new = old + 6",
+            "  Test: divisible by 19",
+            "    If true: throw to monkey 2",
+            "    If false: throw to monkey 0",
+        ]
+        .join(NEW_LINE);
+
+        let result = Monkey::parse(&example);
+
+        assert_eq!(result.items, [54, 65, 75, 74]);
+        assert_eq!((result.operate)(&0), 6);
+        assert_eq!((result.operate)(&6), 12);
+        assert_eq!(result.divisible_num, 19);
+        assert_eq!(result.on_true_target, 2);
+        assert_eq!(result.on_false_target, 0);
+    }
+}
--- src/day_11/part_2.rs
+++ src/day_11/part_2.rs
@@ -1,17 +1,84 @@
-pub fn solve(_input: String) -> String {
-    return "Not solved yet!".to_string();
+use super::commons::Monkey;
+use crate::EMPTY_LINE;
+use std::collections::HashMap;
+
+pub fn solve(input: String) -> String {
+    let mut monkeys: Vec<Monkey> = input
+        .split(EMPTY_LINE)
+        .map(|inp| Monkey::parse(inp))
+        .collect();
+
+    let mut counter: HashMap<usize, usize> = HashMap::new();
+    for _ in 0..10_000 {
+        for index in 0..monkeys.len() {
+            let mut monkey: Monkey = monkeys.remove(index);
+            monkeys.insert(index, Monkey::dummy());
+
+            let count = counter.entry(index).or_insert(0);
+            *count += monkey.items.len();
+
+            for _ in 0..monkey.items.len() {
+                let item = monkey.items.remove(0);
+                let result = (monkey.operate)(&item);
+                if result % monkey.divisible_num == 0 {
+                    monkeys[monkey.on_true_target].items.push(result);
+                } else {
+                    monkeys[monkey.on_false_target].items.push(result);
+                }
+            }
+
+            monkeys.remove(index);
+            monkeys.insert(index, monkey);
+        }
+    }
+
+    let mut values: Vec<&usize> = counter.values().collect();
+    values.sort();
+
+    let result = values.pop().unwrap() * values.pop().unwrap();
+    return result.to_string();
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::NEW_LINE;
 
     #[test]
     fn solve_should_be_correct() {
-        let example = "some-example".to_string();
+        let example = [
+            "Monkey 0:",
+            "  Starting items: 79, 98",
+            "  Operation: new = old * 19",
+            "  Test: divisible by 23",
+            "    If true: throw to monkey 2",
+            "    If false: throw to monkey 3",
+            "",
+            "Monkey 1:",
+            "  Starting items: 54, 65, 75, 74",
+            "  Operation: new = old + 6",
+            "  Test: divisible by 19",
+            "    If true: throw to monkey 2",
+            "    If false: throw to monkey 0",
+            "",
+            "Monkey 2:",
+            "  Starting items: 79, 60, 97",
+            "  Operation: new = old * old",
+            "  Test: divisible by 13",
+            "    If true: throw to monkey 1",
+            "    If false: throw to monkey 3",
+            "",
+            "Monkey 3:",
+            "  Starting items: 74",
+            "  Operation: new = old + 3",
+            "  Test: divisible by 17",
+            "    If true: throw to monkey 0",
+            "    If false: throw to monkey 1",
+        ]
+        .join(NEW_LINE);
 
         let result = solve(example);
 
-        assert_eq!(result, "Not solved yet!")
+        assert_eq!(result, "2713310158")
     }
 }
