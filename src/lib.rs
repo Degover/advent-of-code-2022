ec3bda1b04653fe17dc4952b4e1d5d005841805b
Degover <chaors2000@gmail.com>
2022-12-11 11:17:59 -0300

    Added day 11 to the solution list

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod day_05;
 mod day_06;
 mod day_08;
 mod day_09;
+mod day_11;
 
 pub type SolutionFn = fn(String) -> String;
 
@@ -21,6 +22,7 @@ pub fn get_solution(day: &u8) -> (SolutionFn, SolutionFn) {
         6 => (day_06::part_1::solve, day_06::part_2::solve),
         8 => (day_08::part_1::solve, day_08::part_2::solve),
         9 => (day_09::part_1::solve, day_09::part_2::solve),
+        11 => (day_11::part_1::solve, day_11::part_2::solve),
         _ => {
             println!("This day has no solution");
             (not_implemented, not_implemented)
